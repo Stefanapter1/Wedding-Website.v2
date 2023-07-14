0e6974c2d55b867e3a5038fadbc6bfaa79693e06
brookes22 <brookesb91@hotmail.com>
2023-07-14 12:01:31 +0100

    :wrench: Fixes

--- scripts.js
+++ scripts.js
@@ -1,18 +1,31 @@
-document.querySelector(".spotify1").addEventListener("click", function() {
-    var ieusong = new Audio("sounds/sidimansour.mp3");
-    ieusong.play();
-})
+/**
+ * Reference to audio element.
+ */
+const song = new Audio('sounds/sidimansour.mp3');
 
+document.querySelector('.spotify1').addEventListener('click', function () {
+  /**
+   * If the song is already playing, pause it and seek to the beginning
+   * before playing again.
+   */
 
-var randomWords = [ 'Ieumundo', 'Ewan', 'Flid', 'Ieuan' ];
-var wordDiv = document.getElementsByClassName( 'bestManHead' );
+  song.pause();
+  song.currentTime = 0;
+  song.play();
+});
+
+var randomWords = ['Ieumundo', 'Ewan', 'Flid', 'Ieuan'];
+var wordDiv = document.getElementsByClassName('bestManHead');
 
 function randomWord() {
-    wordDiv.innerHTML = randomWords[ Math.floor(  Math.random() * randomWords.length) ];}
+  wordDiv.innerHTML =
+    randomWords[Math.floor(Math.random() * randomWords.length)];
+}
 
-
-var randomWords2 = [ 'Eefee', 'Aiofe', 'Sister', 'Aiofe' ];
-var wordDiv2 = document.getElementById( 'bridesMaidHead' );
+var randomWords2 = ['Eefee', 'Aiofe', 'Sister', 'Aiofe'];
+var wordDiv2 = document.getElementById('bridesMaidHead');
 
 function randomWord2() {
-    wordDiv2.innerHTML = randomWords2[ Math.floor(  Math.random() * randomWords2.length) ];}
+  wordDiv2.innerHTML =
+    randomWords2[Math.floor(Math.random() * randomWords2.length)];
+}
